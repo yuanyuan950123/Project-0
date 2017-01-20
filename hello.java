8bc128bd79bc45563c40d9ba8a96cc802d736f79
Yuan Yuan <yuanyuan123@berkeley.edu>
2017-01-19 20:21:39 -0800

    Commit our hello.java file

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello{
+	public static void main(String[] args){
+		System.out.println("Hello robin");
+		int sums = sum(2,3);
+		System.out.println(sums);
+	}
+	public static int sum(int a, int b){
+		return a + b;
+	}
+}
